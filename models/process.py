ca4432f1d6c06321de18a54a7b7defa39b4c03cc
Aditi Agarwal <130185768+Agarwal04@users.noreply.github.com>
2025-08-06 02:36:08 +0530

    Update process.py

--- models/process.py
+++ models/process.py
@@ -1,1 +1,13 @@
+class Process:
+    def __init__(self, pid, arrival, burst, priority=0):
+        self.pid = pid
+        self.arrival = arrival
+        self.burst = burst
+        self.remaining = burst
+        self.priority = priority
+        self.completion = 0
+        self.waiting = 0
+        self.turnaround = 0
+        self.start = None  # First time process gets CPU
+ 
  
