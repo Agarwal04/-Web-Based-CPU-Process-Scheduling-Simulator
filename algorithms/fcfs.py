fbd4c914a36ca65567fdfd34ee4d7f23804f7277
Aditi Agarwal <130185768+Agarwal04@users.noreply.github.com>
2025-08-06 02:33:16 +0530

    Update fcfs.py

--- algorithms/fcfs.py
+++ algorithms/fcfs.py
@@ -1,1 +1,22 @@
+def fcfs(processes):
+    processes.sort(key=lambda p: p.arrival)
+    current_time = 0
+    execution_log = []
+
+    for process in processes:
+        if current_time < process.arrival:
+            current_time = process.arrival
+        process.start = current_time
+        start_time = current_time
+        current_time += process.burst
+        end_time = current_time
+
+        process.completion = current_time
+        process.turnaround = process.completion - process.arrival
+        process.waiting = process.turnaround - process.burst
+
+        execution_log.append({'pid': process.pid, 'start': start_time, 'end': end_time})
+
+    return processes, execution_log
+ 
  
